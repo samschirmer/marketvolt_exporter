934c03c844cc1ee235d804d071aaa51d131de2f4
Sam Schirmer <sschirmer@hatchbuck.com>
2020-02-24 17:28:59 -0600

    now splitting into batches and processing much faster; characteristics not fully working

--- db.rb
+++ db.rb
@@ -1,17 +1,35 @@
 class Queryer
-    attr_accessor :db, :accid, :contacts
+    attr_accessor :db, :accid, :contacts, :fields, :chars
 
     def initialize(id)
         creds = Creds.get
         @db = TinyTds::Client.new username: creds[:user], password: creds[:pass], host: creds[:host], database: creds[:db]
         @accid = id
         @contacts = get_contacts
+        @fields = fetch_all_field_names(id)
+        @chars = fetch_all_char_names(id)
+    end
+
+    def fetch_all_field_names(id)
+        names = []
+        sql = "select fieldid, fielddescription from customerfields2 where accountid = #{id}"
+        res = @db.execute(sql)
+        res.each { |r| names << r['fielddescription'] }
+        return names
+    end
+
+    def fetch_all_char_names(id)
+        names = []
+        sql = "select characteristicid, characteristicname from characteristics where deletedfromaccount = 0 and accountid = #{id} order by characteristicname"
+        res = @db.execute(sql)
+        res.each { |r| names << r['characteristicname'] }
+        return names
     end
 
     def get_field_values(id)
         sql = 
             "select cf.fielddescription, d.value
-            from (select fieldid, fielddescription from customerfields2 where accountid = #{@accid}) as cf
+            from (select fieldid, fielddescription from customerfields2 where accountid = #{@accid} and fielddescription not in ('First Name','Last Name','Street Address 1','Street Address 2','City','State','Zip','Date of Birth','Phone Number','Preferred Format','Email Address')) as cf
             left join (select fieldid, value from recipientfielddata where recipientid = #{id}) as d on d.fieldid = cf.fieldid"
         @db.execute(sql)
     end
@@ -25,7 +43,28 @@ class Queryer
         @db.execute(sql)
     end
 
-private
+    def get_batch_field_values(ids)
+            #from (select fieldid, fielddescription from customerfields2 where accountid = #{@accid} and fielddescription not in ('First Name','Last Name','Street Address 1','Street Address 2','City','State','Zip','Date of Birth','Phone Number','Preferred Format','Email Address')) as cf
+        sql = 
+            "select cf.fielddescription, d.value, d.recipientid
+            from (select fieldid, fielddescription from customerfields2 where accountid = #{@accid} and fielddescription not in ('First Name','Last Name','Street Address 1','Street Address 2','City','State','Zip','Date of Birth','Phone Number','Preferred Format','Email Address')) as cf
+            left join (select fieldid, value, recipientid from recipientfielddata where recipientid in (#{ids.join(',')})) as d on d.fieldid = cf.fieldid"
+        @db.execute(sql)
+    end
+
+    def get_batch_customer_characteristics(ids)
+        sql = 
+            "select ch.characteristicid, ch.characteristicname, cc.customerid
+            from (select characteristicid, characteristicname from characteristics where deletedfromaccount = 0 and accountid = #{@accid}) as ch
+            left join (select characteristicid, customerid from customercharacteristics where customerid in (#{ids.join(',')})) as cc on cc.characteristicid = ch.characteristicid
+            order by ch.characteristicname"
+        @db.execute(sql)
+    end
+
+
+    def populate_contacts
+        @contacts = get_contacts
+    end
 
     def get_contacts
         contacts = []
@@ -33,7 +72,7 @@ private
             select customerid, emailaddress, fname, lname, customerstatus, streetaddress1, streetaddress2, city, state, zip, dateofbirth 
             from customers where isnull(deletedfromaccount,0) != 1 and accountid = #{@accid}"
         base_contacts = @db.execute(sql)
-        base_contacts.each(symbolize_keys: true) do |c|
+        base_contacts.each do |c|
             contacts << c 
         end
         return contacts
--- main.rb
+++ main.rb
@@ -1,29 +1,83 @@
 require 'csv'
+require 'thread'
 require 'tiny_tds'
 require './secrets'
 require './db'
 
-print "\naccount id?: "
-accid = $stdin.gets.chomp()
+#print "\naccount id?: "
+# accid = $stdin.gets.chomp()
+accid = 5459312
+#accid = 296299
+
 db = Queryer.new accid
+db.populate_contacts
 
-rows = []
-db.contacts.each_with_index do |row, i|
-    field_values = db.get_field_values(row[:customerid])
-    field_values.each(symbolize_keys: true) { |fv| row.store(fv[:fielddescription], fv[:value]) }
+batch_size = 200
+num_batches = db.contacts.count % batch_size > 0 ? db.contacts.count / batch_size + 1 : db.contacts.count
+batches = []
 
-    char_values = db.get_customer_characteristics(row[:customerid])
-    char_values.each(symbolize_keys: true) { |cv| row.store(cv[:characteristicname], cv[:customerid].nil? ? "" : cv[:characteristicname]) }
-
-    rows << row
-    puts "#{i} of #{db.contacts.count} complete"
+while batches.count < num_batches do 
+    batch = []
+    batch_size.times do 
+        staged = db.contacts.pop
+        batch << staged unless staged.nil?
+    end
+    batches << batch
 end
 
+rows = []
+batches.each_with_index do |batch, i| 
+    ids, records = [], []
+
+    # collecting the ids from this batch and creating empty account object for each one
+    batch.each do |c| 
+        record = { 
+            'id' => c['customerid'],
+            'emailaddress' => c['emailaddress'],
+            'firstname' => c['fname'],
+            'lastname' => c['lname'],
+            'customerstatus' => c['customerstatus'],
+            'streetaddress1' => c['streetaddress1'],
+            'streetaddress2' => c['streetaddress2'],
+            'city' => c['city'],
+            'state' => c['state'],
+            'zip' => c['zip'],
+            'dateofbirth' => c['dateofbirth']
+        }
+        db.fields.each { |field| record.store(field, '') }
+        db.chars.each { |char| record.store(char, '') }
+        pp c if record['id'].to_s == '28218171'
+        pp record if record['id'].to_s == '28218171'
+        records << record
+        ids << c['customerid']
+    end
+
+    # appending field names and values to account (by id)
+    raw_fields = db.get_batch_field_values(ids)
+    raw_fields.each do |fv| 
+        record = records.detect { |a| a['id'] == fv['recipientid'] }
+        record[fv['fielddescription']] = fv['value'] if !record.nil? && fv['recipientid'] == record['id']
+    end
+
+    # appending characteristics to account (by id)
+    raw_chars = db.get_batch_customer_characteristics(ids)
+    raw_chars.each do |cv| 
+        record = records.detect { |a| a['id'] == cv['customerid'] }
+        record[cv['characteristicname']] = cv['characteristicname'] if !record.nil? && cv['recipientid'] == record['id']
+    end
+
+    records.each { |record| rows << record }
+    puts "batch #{i + 1} of #{num_batches} complete (#{batch_size} per batch)"
+end
+    
 if rows.count > 0
+    puts "writing file..."
     filename = "mv_export_#{db.accid}.csv"
     CSV.open(filename, "wb") do |csv|
         csv << rows.first.keys
-        rows.each { |h| csv << h.values }
+        rows.each do |h| 
+            csv << h.values 
+        end
     end
    puts "done: #{filename}"
 else
